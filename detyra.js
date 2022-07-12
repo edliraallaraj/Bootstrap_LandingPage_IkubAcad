e32bd930a9670da8518dfc3f76b017dcae3adcb4
giljanalimani <limanigiljana@gmail.com>
2022-07-12 10:09:20 +0200

    detyra

---
+++ detyra.js
@@ -0,0 +1,24 @@
+//1. “Pi ka vleren 3.14”
+//2.”My favourite programming language is JavaScript” (menyren sesi e aksesoni, zgjidheni ju, array, object, ose ndonje data type tjeter)
+//3. Declare an array with 3 elements, output the forth element and see what happens
+
+
+
+//Ushtrimi 1
+
+let pi = Math.PI
+console.log(pi);
+
+
+//Ushtrimi 2
+
+let language = "JavaScript";
+console.log(`My favourite programming language is ${language}`);
+console.log("My favourite programming language is " + language);
+
+//Ushtrimi 3
+
+let numberArray = [1,2,3];
+console.log(numberArray[4]);
+
+// The output it is undefined because we do not have a forth element.
