6c82841d1920187b43298f2eae499b313a974c36
Your Name <you@example.com>
2022-07-20 18:12:41 +0200

    Landing Page Egli

---
+++ taskWed13.js
@@ -0,0 +1,14 @@
+//Pi ka vleren 3.14
+const pi=3.14;
+let  radius =15;
+let A = pi *(radius+radius);
+console.log("Rrezja e katrorit eshte :",A);
+
+//Afishoni : My favourite programming language is JavaScript
+let Subject= ["C++","PHP","MySQL","JavaScript","Java","Rubby"];
+console.log(` "My favourite programming language is :" ${Subject[3]}`);
+
+//Declare an array with 3 elements, output the fourth element
+//and see what happens
+const car = {type:"Fiat", model:"500", color:"white" , speed:"250km/"};
+console.log("Output forth element :",car.speed);
